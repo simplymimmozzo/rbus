712d94ae484f3f420a9b5e81e692ef7490de4f15
Domenico Musto <domenico.musto@simplybusiness.co.uk>
2017-03-10 13:28:03 +0000

    first implementation of publish, singhe handler

--- lib/rbus.rb
+++ lib/rbus.rb
@@ -2,12 +2,19 @@ require "rbus/version"
 
 module Rbus
   class Bus
+
+
+
     def initialize
 
     end
 
     def add_handler(message_type, handler)
+      @handler = handler
+    end
 
+    def publish(message)
+      @handler.handle message if @handler
     end
   end
 end
--- spec/bus_spec.rb
+++ spec/bus_spec.rb
@@ -1,28 +1,41 @@
 require "spec_helper"
 
-context "when there is an handler configured for a message" do
+class TestEvent
 
-  class TestEvent
+end
 
+class TestEventHandler
+
+  attr_reader :handled
+  @handled
+
+  def handle(message)
+    @handled = true
   end
+end
 
-  class TestEventHandler
-
-    attr_reader :handled
-    @handled
-
-    def handle(message)
-      @handled = true
-    end
-  end
+context "when an event is published and there is an handler configured for it" do
 
   before do
     bus = Rbus::Bus.new
     @test_event_handler = TestEventHandler.new
     bus.add_handler(TestEvent, @test_event_handler)
+    bus.publish TestEvent.new
   end
 
-  it "should dispatch the message to the confugured handler" do
-    expect(@test_event_handler.handled).to be_true
+  it "should dispatch the message to the configured handler" do
+    expect(@test_event_handler.handled).to be true
+  end
+end
+
+context "when an event is published and there is no handler configured for it" do
+
+  before do
+    @bus = Rbus::Bus.new
+    @test_event_handler = TestEventHandler.new
+  end
+
+  it "should not complain about it" do
+    expect{@bus.publish TestEvent.new}.to_not raise_error
   end
 end
