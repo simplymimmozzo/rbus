938ff574a7d14d108d6e33101219b890d56324c1
Domenico Musto <domenico.musto@simplybusiness.co.uk>
2017-03-10 14:04:52 +0000

    added support for multiple handlers

--- lib/rbus.rb
+++ lib/rbus.rb
@@ -3,18 +3,25 @@ require "rbus/version"
 module Rbus
   class Bus
 
-
+    @handlers
 
     def initialize
-
+      @handlers = Hash.new
     end
 
     def add_handler(message_type, handler)
-      @handler = handler
+      @handlers[message_type.name] = [] unless @handlers[message_type.name]
+      @handlers[message_type.name] << handler
     end
 
     def publish(message)
-      @handler.handle message if @handler
+      return if !@handlers[message.class.name]
+      handlers = @handlers[message.class.name]
+      return if handlers.count == 0
+      handlers.each do |handler|
+        handler.handle message
+      end
     end
   end
+
 end
--- spec/bus_spec.rb
+++ spec/bus_spec.rb
@@ -12,7 +12,10 @@ end
 class TestEventHandler
 
   attr_reader :received_message
-  @received_message
+
+  def initialize
+    @received_message = false
+  end
 
   def handle(message)
     @received_message = true
@@ -22,10 +25,13 @@ end
 class AnotherTestEventHandler
 
   attr_reader :received_message
-  @received_message
+
+  def initialize
+    @received_message = false
+  end
 
   def handle(message)
-    received_message = true
+    @received_message = true
   end
 end
 
@@ -46,6 +52,25 @@ context "when an event is published and there is an handler configured for it" d
 
   it "should not dispatch the message to a non relevant handler" do
     expect(@another_test_event_handler.received_message).to be false
+  end
+
+end
+
+
+context "when an event is published and there is more than one handler configured for it" do
+
+  before do
+    bus = Rbus::Bus.new
+    @test_event_handler = TestEventHandler.new
+    @another_test_event_handler = AnotherTestEventHandler.new
+    bus.add_handler(TestEvent, @test_event_handler)
+    bus.add_handler(TestEvent, @another_test_event_handler)
+    bus.publish TestEvent.new
+  end
+
+  it "should dispatch the message to all configured handlers" do
+    expect(@test_event_handler.received_message).to be true
+    expect(@another_test_event_handler.received_message).to be true
   end
 
 end
