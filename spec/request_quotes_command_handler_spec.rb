fd4a713b49e5fa16ef220e820e5fde140ad745bf
Domenico Musto <domenico.musto@simplybusiness.co.uk>
2017-03-14 16:06:16 +0000

    using have_received

--- spec/request_quotes_command_handler_spec.rb
+++ spec/request_quotes_command_handler_spec.rb
@@ -4,10 +4,10 @@ context "when handling a request quotes command" do
 
   before do
     @bus = instance_double("Rbus::Bus")
-    #allow(@bus).to receive(:publish)
+    allow(@bus).to receive(:publish)
     @command = {:policyId => 1}
     @handler = Handlers::RequestQuotesCommandHandler.new(@bus)
-
+    @handler.handle(@command)
   end
 
   it 'should generate the quotes' do
@@ -16,8 +16,7 @@ context "when handling a request quotes command" do
 
   it "should publish a quotes generated event" do
     #expect(@bus).to receive(:publish).with({:quotes => [1,2,3]})
-    expect(@bus).to receive(:publish).with(kind_of(Handlers::QuotesGeneratedEvent))
-    @handler.handle(@command)
+    expect(@bus).to have_received(:publish).with(kind_of(Handlers::QuotesGeneratedEvent))
   end
 
   class RequestQuotesCommand
