cbbd115a579f329c6a7ac38fbb0dc2b30cfdeb55
Domenico Musto <domenico.musto@simplybusiness.co.uk>
2017-03-14 15:55:44 +0000

    experimental

--- spec/request_quotes_command_handler_spec.rb
+++ spec/request_quotes_command_handler_spec.rb
@@ -4,6 +4,7 @@ context "when handling a request quotes command" do
 
   before do
     @bus = instance_double("Rbus::Bus")
+    #allow(@bus).to receive(:publish)
     @command = {:policyId => 1}
     @handler = Handlers::RequestQuotesCommandHandler.new(@bus)
 
@@ -17,5 +18,9 @@ context "when handling a request quotes command" do
     #expect(@bus).to receive(:publish).with({:quotes => [1,2,3]})
     expect(@bus).to receive(:publish).with(kind_of(Handlers::QuotesGeneratedEvent))
     @handler.handle(@command)
+  end
+
+  class RequestQuotesCommand
+
   end
 end
