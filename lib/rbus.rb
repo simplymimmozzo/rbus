bf480c270856d108ef2bd46cd9cd05c554fda1dc
Domenico Musto <domenico.musto@simplybusiness.co.uk>
2017-03-16 09:52:31 +0000

    refactoring

--- lib/rbus.rb
+++ lib/rbus.rb
@@ -3,29 +3,27 @@ require "rbus/version"
 module Rbus
   class Bus
 
-    @handlers
-
     def initialize
       @handlers = Hash.new
     end
 
     def add_handler(message_type, handler)
       raise BusConfigurationError, "Invalid nil handler instance" if ! handler
-      @handlers[message_type.name] = [] unless @handlers[message_type.name]
-      @handlers[message_type.name] << handler
+      @handlers[message_type] = [] unless @handlers[message_type]
+      @handlers[message_type] << handler
     end
 
     def publish(message)
-      return if !@handlers[message.class.name]
-      @handlers[message.class.name].each do |handler|
+      return if !@handlers[message.class]
+      @handlers[message.class].each do |handler|
         handler.handle message
       end
     end
 
     def send(message)
-      raise BusConfigurationError, "No handler configured for command #{message.class.name}" if !@handlers[message.class.name]
-      raise BusConfigurationError, "Too many handlers configured for command #{message.class.name}, only 1 is allowed" if @handlers[message.class.name].count != 1
-      handler = @handlers[message.class.name].first
+      raise BusConfigurationError, "No handler configured for command #{message.class.name}" if !@handlers[message.class]
+      raise BusConfigurationError, "Too many handlers configured for command #{message.class.name}, only 1 is allowed" if @handlers[message.class].count != 1
+      handler = @handlers[message.class].first
       handler.handle message
     end
   end
